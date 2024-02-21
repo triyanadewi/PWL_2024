c05f1ebb045a8dea968bce342351c97c8b9d876c
triyanadewi <triyanadewifatmawati@gmail.com>
2024-02-21 19:49:43 +0700

    Kembali Semula

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,9 +8,5 @@
 <body>
     <h2>Pemrograman Web Lanjut 2024</h2>
     <h3>Semangat Belajar Laravel 10</h3>
-    <p>Perkenalkan Nama Saya Triyana Dewi Fatmawati. Saya biasa dipanggil Triyana.<br>
-        Saya berkuliah di Politeknik Negeri Malang.<br>
-        Saya mengambil Jurusan Teknologi Informasi dengan Program Studi D4 Teknik Informatika.<br>
-        Saat ini saya sudah semester 4.</p>
 </body>
 </html>
