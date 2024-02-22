c94984c4c82e3f37dc89c7b1e5d9a81f4f57511f
triyanadewi <triyanadewifatmawati@gmail.com>
2024-02-22 15:14:36 +0700

    Praktikum Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Basic Routing
+Route::get('/hello', function(){
+    return 'Hello World';
+});
+
+Route::get('/world', function () { 
+    return 'World';
+});
+
+Route::get('/', function () { 
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () { 
+    return 'NIM: 2241720206' . '<br>' . 'Nama: Triyana Dewi Fatmawati';
+});
+
+// Route Parameters
+Route::get('/user/{name}', function ($name) { 
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+    
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+// Optional Parameters
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
